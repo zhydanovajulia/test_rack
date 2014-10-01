1d4b4c9013479a98e4a8d147d0374d5f095dde27
Julia Zhydanova <jz@cssum.net>
2014-10-01 22:23:32 +0300

    test

--- socket_server.rb
+++ socket_server.rb
@@ -1,5 +1,15 @@
+require 'socket'
+
 class SocketServer
   def call(env)
-    [200, {"Content-Type" => "text/html"}, ["Hello world!!!!!!!!!!"]]
+   # server = TCPSocket.new 'localhost', 2000 # Server bound to port 2000
+
+    #loop do
+    #  client = server.accept    # Wait for a client to connect
+    #  client.puts "Hello !"
+    #  client.puts "Time is #{Time.now}"
+    #  client.close
+    #end
+   [200, {"Content-Type" => "text/html"}, ["Hello world!!!!!!!!!!"]]
   end
 end
